aa0d77397b4598c52cae1f89a8f9d9f82a616945
Diyas <dias.tursunkali@gmail.com>
2020-10-24 17:09:17 +0600

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BooksComponent} from './books/books.component';
+import {AuthorsComponent} from './authors/authors.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './home/login/login.component';
+import {RegistrationComponent} from './home/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'authors',
+    component: AuthorsComponent
+  },
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'regist',
+    component: RegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { BookComponent } from './books/book/book.component';
+import { AboutComponent } from './home/about/about.component';
+import { AuthorComponent } from './authors/author/author.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegistrationComponent } from './home/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthorsComponent,
+    BooksComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookComponent,
+    AboutComponent,
+    AuthorComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
