a721ff1cd14ba80626770125358d916cb3bf4ab2
Diyas <dias.tursunkali@gmail.com>
2020-11-07 17:36:18 +0600

    1

---
+++ src/app/books/add-new-book/add-new-book.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {BookLogging} from './BookService/bookLogging';
+
+@Component({
+  selector: 'app-add-new',
+  templateUrl: './add-new.component.html',
+  styleUrls: ['./add-new.component.css']
+})
+export class AddNewBookComponent implements OnInit {
+
+  closeResult = '';
+  constructor(private modalService: NgbModal) {}
+
+  // tslint:disable-next-line:typedef
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  @Output() newItemEvent = new EventEmitter<any>();
+  @Input() list: [];
+  // tslint:disable-next-line:typedef
+  addNewBook(name: string, author: string, likes: string)  {
+    this.newItemEvent.emit({name, author, likes});
+
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  ngOnInit(): void {
+  }
+}
